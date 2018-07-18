a6ef50d60595f6485f6026ecd08240da426129da
emanuelhuber <emanuel.huber@tranquille.ch>
2018-07-18 17:46:59 +0200

    fix bug

--- R/main.R
+++ R/main.R
@@ -720,6 +720,7 @@ setAs(from = "Deposits2D", to = "matrix", def = function(from){
     # layID <- sapply(from@layers, function(x) x[['id']])
     # sel <- which(sapply(layID, function(x) length(x) > 0))
     sel <- sapply(from@layers, function(x) !is.null(x[['obj']]))
+    layID <- sapply(from@layers[sel], function(x) x[['id']])
     obj <- lapply(from@layers[sel], function(x) as.matrix(x[['obj']]))
     objlayID <- rep(layID, sapply(obj, nrow))
     M <- matrix(nrow = length(objlayID), ncol = 7)
